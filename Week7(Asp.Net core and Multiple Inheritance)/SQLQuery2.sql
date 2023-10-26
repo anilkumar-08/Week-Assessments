9a38c2d9d6fb47e10d54dbdc74e10a1723cec035
anilkumar-08 <106294572+anilkumar-08@users.noreply.github.com>
2023-10-27 00:17:55 +0530

    7.1
    Asp.Net core

---
+++ Week7(Asp.Net core and Multiple Inheritance)/SQLQuery2.sql
@@ -0,0 +1,8 @@
+﻿create table Passenger
+(
+PassengerID int primary key not null,
+ReservationID int foreign key references Reservations(Reservationid),
+PassengerName varchar(100) ,
+);
+
+drop table Passenger
