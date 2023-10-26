68688a8aa91d5965a7184dbfd709f6503ea23a1c
anilkumar-08 <106294572+anilkumar-08@users.noreply.github.com>
2023-10-27 01:38:33 +0530

    7.2
    Multiple Inheritance with Interface

---
+++ Week7(Asp.Net core and Multiple Inheritance)/Multiple Inheritance/IGoldLoanAccount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace MultipleInheritancewithInterface
+{
+    public interface IGoldLoanAccount
+    {
+        double CalculateGoldInterest(double gbalance);
+    }
+}
+
+
+
